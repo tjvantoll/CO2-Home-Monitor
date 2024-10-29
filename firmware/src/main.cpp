de5f8d39490a80bc2c0dad18d9c02a32fd57d7fe
TJ VanToll <tj.vantoll@gmail.com>
2024-10-29 16:05:22 -0400

    more production friendly firmware

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -3,7 +3,7 @@
 #include <SensirionI2CScd4x.h>
 #include <Wire.h>
 
-#define productUID "com.blues.tvantoll:co2_home_monitor"
+#define DEVELOPMENT false
 
 Notecard notecard;
 SensirionI2CScd4x scd4x;
@@ -30,7 +30,7 @@ float getVoltage()
   float voltage;
   J *req = notecard.newRequest("card.voltage");
 
-  if (J *rsp = notecard.requestAndResponse(req))
+  if (J *rsp = notecard.requestAndResponseWithRetry(req, 5))
   {
     voltage = JGetNumber(rsp, "value");
     notecard.deleteResponse(rsp);
@@ -44,74 +44,55 @@ void setup()
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
 
-  static const size_t MAX_SERIAL_WAIT_MS = 5000;
-  size_t begin_serial_wait_ms = ::millis();
-  // Wait for the serial port to become available
-  while (!Serial && (MAX_SERIAL_WAIT_MS > (::millis() - begin_serial_wait_ms)));
-  Serial.begin(115200);
-  notecard.setDebugOutputStream(Serial);
+  #if DEVELOPMENT
+    static const size_t MAX_SERIAL_WAIT_MS = 5000;
+    size_t begin_serial_wait_ms = ::millis();
+    // Wait for the serial port to become available
+    while (!Serial && (MAX_SERIAL_WAIT_MS > (::millis() - begin_serial_wait_ms)));
+    Serial.begin(115200);
+    notecard.setDebugOutputStream(Serial);
+  #else
+    Serial.end();
+  #endif
+
+  delay(10000);
 
   notecard.begin();
   Wire.begin();
   scd4x.begin(Wire);
 
   uint16_t error;
-  char errorMessage[256];
 
-  uint16_t serial0;
-  uint16_t serial1;
-  uint16_t serial2;
-  error = scd4x.getSerialNumber(serial0, serial1, serial2);
-  if (error) {
-    Serial.print("Error trying to execute getSerialNumber(): ");
-    errorToString(error, errorMessage, 256);
-    Serial.println(errorMessage);
-  } else {
-    printSerialNumber(serial0, serial1, serial2);
-  }
+  #if DEVELOPMENT
+    char errorMessage[256];
 
-  {
-    // Configure the Notecard
-    J *req = notecard.newRequest("hub.set");
-    JAddStringToObject(req, "product", productUID);
-    JAddStringToObject(req, "mode", "periodic");
-    JAddStringToObject(req, "voutbound", "usb:1;high:60;normal:120;low:360;dead:0");
-    JAddStringToObject(req, "vinbound", "usb:1;high:1440;normal:1440;low:1440;dead:0");
-    notecard.sendRequest(req);
-  }
-
-  {
-    // Configure a template for the data
-    J *req = notecard.newRequest("note.template");
-    JAddStringToObject(req, "file", "data.qo");
-    JAddNumberToObject(req, "port", 1);
-    JAddStringToObject(req, "format", "compact");
-    J *body = JCreateObject();
-    JAddNumberToObject(body, "co2", 12);
-    JAddNumberToObject(body, "temp", 14.1);
-    JAddNumberToObject(body, "humidity", 14.1);
-    JAddNumberToObject(body, "_time", 14);
-    JAddNumberToObject(body, "voltage", 14.1);
-    JAddItemToObject(req, "body", body);
-    notecard.sendRequest(req);
-  }
-
-  {
-    // Optimize power usage for LiPo battery
-    J *req = notecard.newRequest("card.voltage");
-    JAddStringToObject(req, "mode", "lipo");
-    notecard.sendRequest(req);
-  }
+    uint16_t serial0;
+    uint16_t serial1;
+    uint16_t serial2;
+    error = scd4x.getSerialNumber(serial0, serial1, serial2);
+    if (error) {
+      Serial.print("Error trying to execute getSerialNumber(): ");
+      errorToString(error, errorMessage, 256);
+      Serial.println(errorMessage);
+    } else {
+      printSerialNumber(serial0, serial1, serial2);
+    }
+  #endif
 
   // Start Measurement
   error = scd4x.startPeriodicMeasurement();
-  if (error) {
-    Serial.print("Error trying to execute startPeriodicMeasurement(): ");
-    errorToString(error, errorMessage, 256);
-    Serial.println(errorMessage);
-  }
+  #if DEVELOPMENT
+    if (error) {
+      Serial.print("Error trying to execute startPeriodicMeasurement(): ");
+      errorToString(error, errorMessage, 256);
+      Serial.println(errorMessage);
+    }
+  #endif
 
-  Serial.println("Waiting for first measurement... (5 sec)");
+  #if DEVELOPMENT
+    Serial.println("Waiting for first measurement... (5 sec)");
+  #endif
+
   delay(5000);
 
   // Read Measurement
@@ -121,22 +102,25 @@ void setup()
 
   // From https://learn.adafruit.com/adafruit-scd-40-and-scd-41/arduino
   error = scd4x.readMeasurement(co2, temp, humidity);
-  if (error) {
-    Serial.print("Error trying to execute readMeasurement(): ");
-    errorToString(error, errorMessage, 256);
-    Serial.println(errorMessage);
-  } else if (co2 == 0) {
-    Serial.println("Invalid sample detected, skipping.");
-  } else {
-    Serial.print("Co2:");
-    Serial.print(co2);
-    Serial.print("\t");
-    Serial.print("Temperature:");
-    Serial.print(temp);
-    Serial.print("\t");
-    Serial.print("Humidity:");
-    Serial.println(humidity);
-  }
+
+  #if DEVELOPMENT
+    if (error) {
+      Serial.print("Error trying to execute readMeasurement(): ");
+      errorToString(error, errorMessage, 256);
+      Serial.println(errorMessage);
+    } else if (co2 == 0) {
+      Serial.println("Invalid sample detected, skipping.");
+    } else {
+      Serial.print("Co2:");
+      Serial.print(co2);
+      Serial.print("\t");
+      Serial.print("Temperature:");
+      Serial.print(temp);
+      Serial.print("\t");
+      Serial.print("Humidity:");
+      Serial.println(humidity);
+    }
+  #endif
 
   {
     J *req = notecard.newRequest("note.add");
@@ -151,16 +135,18 @@ void setup()
         JAddNumberToObject(body, "humidity", humidity);
         JAddNumberToObject(body, "voltage", getVoltage());
       }
-      notecard.sendRequest(req);
+      notecard.sendRequestWithRetry(req, 5);
     }
   }
 
   error = scd4x.stopPeriodicMeasurement();
-  if (error) {
-    Serial.print("Error trying to execute stopPeriodicMeasurement(): ");
-    errorToString(error, errorMessage, 256);
-    Serial.println(errorMessage);
-  }
+  #if DEVELOPMENT
+    if (error) {
+      Serial.print("Error trying to execute stopPeriodicMeasurement(): ");
+      errorToString(error, errorMessage, 256);
+      Serial.println(errorMessage);
+    }
+  #endif
 }
 
 void loop()
